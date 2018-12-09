d9e3530a998d1a7af95aeecc6e1abe3d68a8d4a7
Martin Fouilleul <martinfouilleul@gmail.com>
2018-12-10 00:41:02 +0100

    Show spectrograms in griffin lim test scripts

--- src/util/griffin_lim.test.py
+++ src/util/griffin_lim.test.py
@@ -8,15 +8,15 @@ fe = 44100.
 length_seconds = 0.5
 n_samples = int(fe * length_seconds)
 
-f0 = 440. / fe
+f0 = 3000. / fe
 
-x_ref = np.sin(2 * np.pi * f0 * np.arange(0, n_samples))
-x_ref += np.sin(2 * np.pi * 2.5 * f0 * np.arange(0, n_samples) + np.pi / 2)
-x_ref += np.sin(2 * np.pi * 3.2 * f0 * np.arange(0, n_samples) + np.pi / 3)
+x_ref = 1 * np.sin(2 * np.pi * f0 * np.arange(0, n_samples))
+x_ref += 0.5* np.sin(2 * np.pi * 2 * f0 * np.arange(0, n_samples))
+x_ref += 0.5 * np.sin(2 * np.pi * 3 * f0 * np.arange(0, n_samples))
 
 # Compute stft and stft magnitude
 
-n_fft = 2048
+n_fft = 512
 hop_size = int(np.floor(n_fft / 2))
 window = np.sqrt(np.hanning(n_fft))
 
@@ -25,14 +25,22 @@ spectrum_mag = np.absolute(spectrum_ref)
 
 x_reconstruct_ref = gl.griffin_lim_istft(spectrum_ref, window, hop_size)
 
+plt.imshow(spectrum_mag, origin='lower', extent=(0, length_seconds * 2000, 0, n_fft/2 * 4))
+plt.show()
+
 # Reconstruct x from the stft magnitude
 
 x_reconstruct = gl.griffin_lim_reconstruct(
     spectrum_mag, window, hop_size, 1000)
+
+s_reconstruct = gl.griffin_lim_stft(x_reconstruct, window, hop_size)
 
 # Plot the reconstruction against the reference
 
 plt.plot(x_ref, 'r')
 plt.plot(x_reconstruct_ref, 'g')
 plt.plot(x_reconstruct, 'b')
+
+plt.figure()
+plt.imshow(np.absolute(s_reconstruct), origin='lower', extent=(0, length_seconds * 2000, 0, n_fft/2  * 4))
 plt.show()
