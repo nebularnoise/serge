7196e66822c7dd84c15c4267746d7253955c4bca
Antoine <caillonantoine@gmail.com>
2018-12-16 14:55:06 +0100

    Div bench

---
+++ notebooks/MMDs.py
@@ -0,0 +1,132 @@
+#### EXERCISE
+
+# take your RBF-MMD definition and these two other definitions
+# then compare them and make sure they work as expected; I propose the following
+
+# you should have a unit gaussian prior sampling function
+# you should have a gaussian distribution sampler with varying mean and variance
+# then you sample gaussians with varying means and variances and calculate the MMDs against the prior
+
+# while you move the mean away from 0 and/or the variance away from one (either to 0 or to +inf)
+# then the MMD should score larger against the fixed unit gaussian prior
+
+# you should then have plots showing the behavior of each MMD according
+# to different means/variances of the gaussian tested against the prior
+# and when you sample the gaussian with 0-mean unit variance, you should have a MMD close to 0
+# (as you sample the same distribution as the prior)
+
+# below formulas for each
+# both regularizers take batches of input vectors with same shape (batch_size x vector dimensions)
+
+
+#### RBF MMD regularizer
+# instead, based on https://github.com/OctoberChang/MMD-GAN
+
+def _mmd2(K_XX, K_XY, K_YY, const_diagonal=False, biased=False):
+    m = K_XX.size(0)    # assume X, Y are same shape
+
+    # Get the various sums of kernels that we'll use
+    # Kts drop the diagonal, but we don't need to compute them explicitly
+    if const_diagonal is not False:
+        diag_X = diag_Y = const_diagonal
+        sum_diag_X = sum_diag_Y = m * const_diagonal
+    else:
+        diag_X = torch.diag(K_XX)                       # (m,)
+        diag_Y = torch.diag(K_YY)                       # (m,)
+        sum_diag_X = torch.sum(diag_X)
+        sum_diag_Y = torch.sum(diag_Y)
+
+    Kt_XX_sums = K_XX.sum(dim=1) - diag_X             # \tilde{K}_XX * e = K_XX * e - diag_X
+    Kt_YY_sums = K_YY.sum(dim=1) - diag_Y             # \tilde{K}_YY * e = K_YY * e - diag_Y
+    K_XY_sums_0 = K_XY.sum(dim=0)                     # K_{XY}^T * e
+
+    Kt_XX_sum = Kt_XX_sums.sum()                       # e^T * \tilde{K}_XX * e
+    Kt_YY_sum = Kt_YY_sums.sum()                       # e^T * \tilde{K}_YY * e
+    K_XY_sum = K_XY_sums_0.sum()                       # e^T * K_{XY} * e
+
+    if biased:
+        mmd2 = ((Kt_XX_sum + sum_diag_X) / (m * m)
+            + (Kt_YY_sum + sum_diag_Y) / (m * m)
+            - 2.0 * K_XY_sum / (m * m))
+    else:
+        mmd2 = (Kt_XX_sum / (m * (m - 1))
+            + Kt_YY_sum / (m * (m - 1))
+            - 2.0 * K_XY_sum / (m * m))
+
+    return mmd2
+
+def _mix_rbf_kernel(X, Y, sigma_list):
+    assert(X.size(0) == Y.size(0))
+    m = X.size(0)
+
+    Z = torch.cat((X, Y), 0)
+    ZZT = torch.mm(Z, Z.t())
+    diag_ZZT = torch.diag(ZZT).unsqueeze(1)
+    Z_norm_sqr = diag_ZZT.expand_as(ZZT)
+    exponent = Z_norm_sqr - 2 * ZZT + Z_norm_sqr.t()
+
+    K = 0.0
+    for sigma in sigma_list:
+        gamma = 1.0 / (2 * sigma**2)
+        K += torch.exp(-gamma * exponent)
+
+    return K[:m, :m], K[:m, m:], K[m:, m:], len(sigma_list)
+
+def mix_rbf_mmd2(X, Y, sigma_list=[1, 2, 4, 8, 16], biased=True):
+    K_XX, K_XY, K_YY, d = _mix_rbf_kernel(X, Y, sigma_list)
+    return _mmd2(K_XX, K_XY, K_YY, const_diagonal=False, biased=biased)
+
+#MMDloss = mix_rbf_mmd2(z_encoder,z_prior,sigma_list=[sigmas]).sum()
+# should be descended in optimization and the sigmas are RBF kernel parameters
+# each parameter is for one kernel, all kernels are summed
+
+
+#### inverse multi-quadratic MMD regularizer (for isotropic Gaussian prior only ?)
+# don't remember the source
+
+def im_kernel_sum(z1, z2, z_var, exclude_diag=True):
+    r"""Calculate sum of sample-wise measures of inverse multiquadratics kernel described in the WAE paper.
+    Args:
+        z1 (Tensor): batch of samples from a multivariate gaussian distribution \
+            with scalar variance of z_var.
+        z2 (Tensor): batch of samples from another multivariate gaussian distribution \
+            with scalar variance of z_var.
+        exclude_diag (bool): whether to exclude diagonal kernel measures before sum it all.
+    """
+    assert z1.size() == z2.size()
+    assert z1.ndimension() == 2
+
+    z_dim = z1.size(1)
+    C = 2*z_dim*z_var
+
+    z11 = z1.unsqueeze(1).repeat(1, z2.size(0), 1)
+    z22 = z2.unsqueeze(0).repeat(z1.size(0), 1, 1)
+
+    kernel_matrix = C/(1e-9+C+(z11-z22).pow(2).sum(2))
+    kernel_sum = kernel_matrix.sum()
+    # numerically identical to the formulation. but..
+    if exclude_diag:
+        kernel_sum -= kernel_matrix.diag().sum()
+
+    return kernel_sum
+
+def imq_mmd(z_tilde, z, z_var):
+    r"""Calculate maximum mean discrepancy described in the WAE paper.
+    Args:
+        z_tilde (Tensor): samples from deterministic non-random encoder Q(Z|X).
+            2D Tensor(batch_size x dimension).
+        z (Tensor): samples from prior distributions. same shape with z_tilde.
+        z_var (Number): scalar variance of isotropic gaussian prior P(Z).
+    """
+    assert z_tilde.size() == z.size()
+    assert z.ndimension() == 2
+
+    n = z.size(0)
+    out = im_kernel_sum(z, z, z_var, exclude_diag=True).div(n*(n-1)) + \
+          im_kernel_sum(z_tilde, z_tilde, z_var, exclude_diag=True).div(n*(n-1)) + \
+          -im_kernel_sum(z, z_tilde, z_var, exclude_diag=False).div(n*n).mul(2)
+
+    return out
+
+# MMDloss = imq_mmd(z_encoder,z_prior,1)
+# should be descended in optimization and here for unit variance isotropic gaussian prior
