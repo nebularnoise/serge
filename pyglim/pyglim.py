0f8af8135d20950672c7981b6d9d45a64013b33e
Martin Fouilleul <martinfouilleul@gmail.com>
2019-01-20 17:08:08 +0100

    Added a python wrapper around the C implementation of the Griffin-Lim algorithm

---
+++ pyglim/pyglim.py
@@ -0,0 +1,102 @@
+"""
+
+A python wrapper around a C implementation of the Griffin-Lim algorithm.
+
+Author : Martin Fouilleul
+
+"""
+
+import numpy as np
+import ctypes as C
+
+libglim = C.cdll.LoadLibrary('lib/libglim.dylib')
+
+libglim.GriffinLimReconstruct.argtypes = (C.c_int, C.c_int, C.c_int, C.c_int, np.ctypeslib.ndpointer(
+    dtype=np.float64), C.c_double, np.ctypeslib.ndpointer(dtype=np.float64), np.ctypeslib.ndpointer(dtype=np.float64))
+
+libglim.GriffinLimReconstructFloat.argtypes = (C.c_int, C.c_int, C.c_int, C.c_int, np.ctypeslib.ndpointer(
+    dtype=np.float32), C.c_float, np.ctypeslib.ndpointer(dtype=np.float32), np.ctypeslib.ndpointer(dtype=np.float32))
+
+
+def griffin_lim_reconstruct(iteration_count, spectrogram_mag, window, window_gain, hop_size):
+    """Griffin-Lim Reconstruction
+
+    Reconstructs a signal from a magnitude spectrogram. Uses double precision floating point numbers
+
+    Parameters
+    ----------
+    iteration_count: integer
+	Number of iterations of the Griffin-Lim algorithm
+
+    spectrogram_mag: list or numpy array
+        Input magnitude spectrogram. The rows are consecutive time slices, the columns are the fft frequency bins (up to fft_size/2 + 1)
+
+    window: list of numpy array
+        Window to use during analysis and overlap-add reconstruction
+
+    window_gain: float
+        Gain resulting from applying a weighted overlap add of the window at the specified hop size
+
+    hop_size: integer
+	Number of samples between two consecutive time slices
+
+    Returns
+    -------
+    numpy array
+        Reconstructed signal
+    """
+
+    fft_size = len(window)
+    slice_count = spectrogram_mag.shape[0]
+
+    spectrogram_buffer = np.asarray(spectrogram_mag, dtype=np.float64)
+    window_buffer = np.asarray(window, dtype=np.float64)
+    signal_buffer = np.empty(
+        (slice_count - 1) * hop_size + fft_size, dtype=np.float64)
+
+    libglim.GriffinLimReconstruct(C.c_int(iteration_count), C.c_int(fft_size), C.c_int(hop_size), C.c_int(
+        slice_count), window_buffer, C.c_double(window_gain), spectrogram_buffer, signal_buffer)
+
+    return signal_buffer
+
+
+def griffin_lim_reconstruct_single_precision(iteration_count, spectrogram_mag, window, window_gain, hop_size):
+    """Griffin-Lim Reconstruction
+
+    Reconstructs a signal from a magnitude spectrogram. This version uses single precision floating point numbers
+
+    Parameters
+    ----------
+    iteration_count: integer
+	Number of iterations of the Griffin-Lim algorithm
+
+    spectrogram_mag: list or numpy array
+        Input magnitude spectrogram. The rows are consecutive time slices, the columns are the fft frequency bins (up to fft_size/2 + 1)
+
+    window: list of numpy array
+        Window to use during analysis and overlap-add reconstruction
+
+    window_gain: float
+        Gain resulting from applying a weighted overlap add of the window at the specified hop size
+
+    hop_size: integer
+	Number of samples between two consecutive time slices
+
+    Returns
+    -------
+    numpy array
+        Reconstructed signal
+    """
+
+    fft_size = len(window)
+    slice_count = spectrogram_mag.shape[0]
+
+    spectrogram_buffer = np.asarray(spectrogram_mag, dtype=np.float32)
+    window_buffer = np.asarray(window, dtype=np.float32)
+    signal_buffer = np.empty(
+        (slice_count - 1) * hop_size + fft_size, dtype=np.float32)
+
+    libglim.GriffinLimReconstructFloat(C.c_int(iteration_count), C.c_int(fft_size), C.c_int(hop_size), C.c_int(
+        slice_count), window_buffer, C.c_float(window_gain), spectrogram_buffer, signal_buffer)
+
+    return signal_buffer
